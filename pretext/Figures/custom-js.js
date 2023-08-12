23d3583516d6cd65552c84c697b07e7aa0f6d06a
Beryl Hoffman <hoffmanb@elms.edu>
2023-08-12 18:03:44 -0400

    Some style changes to widen activecode

--- pretext/Figures/custom-js.js
+++ pretext/Figures/custom-js.js
@@ -1,8 +1,8 @@
 // Custom js file
 // Change text of buttons
-var prevbtn = document.querySelector('.previous-button');
-var nextbtn = document.querySelector('.next-button');
-var upbtn = document.querySelector('.up-button');
+const prevbtn = document.querySelector('.previous-button');
+const nextbtn = document.querySelector('.next-button');
+const upbtn = document.querySelector('.up-button');
 prevbtn.style.content = 'none'; // clear ::after that pretext adds - but didn't work
 prevbtn.textContent = '<';
 nextbtn.style.content = 'none';
@@ -11,12 +11,15 @@ upbtn.style.content = 'none';
 upbtn.textContent = '^';
 
 document.onreadystatechange = function () {
-    if (document.readyState == "complete") 
+  /* if (document.readyState == "complete") 
        if (typeof toggletoc !== undefined) 
-          toggletoc();  // pretext v. 0.2, on Runestone   
-       else if (window.mathbook != null)
-          window.mathbook.toggleSidebarLeft(false); // pretext v. 0.13, in local versi
-}
+          toggletoc();    */
+     
+   // hide navbar - hides but leaves space
+   // document.querySelector("ptx-sidebar").className = "hidden";
+
+}   
+       
 
 
 
